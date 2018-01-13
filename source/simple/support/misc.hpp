ce9a93def199c832099bbb9dbba0b7490a06c92f
namark <nshan.nnnn@gmail.com>
2018-01-13 04:11:37 +0400

    String to number and numeric range parsers.

---
+++ source/simple/support/misc.hpp
@@ -0,0 +1,116 @@
+#ifndef SIMPLE_SUPPORT_MISC_CPP
+#define SIMPLE_SUPPORT_MISC_CPP
+
+#include <type_traits>
+#include <limits>
+#include <stdexcept>
+#include <string>
+#include <istream>
+
+#include "range.hpp"
+
+namespace simple::support
+{
+
+	template <typename N>
+	N strton(const char* str, char ** end = nullptr)
+	{
+		static_assert(std::is_arithmetic_v<N>, "simple::support::strton expects an arithmetic type!");
+		if constexpr (std::is_same_v<float, N>)
+			return std::strtof(str, end);
+		else if constexpr (std::is_same_v<double, N>)
+			return std::strtod(str, end);
+		else if constexpr (std::is_same_v<long double, N>)
+			return std::strtold(str, end);
+		else if constexpr (std::is_same_v<long, N>)
+			return std::strtol(str, end, 0);
+		else if constexpr (std::is_same_v<long long, N>)
+			return std::strtoll(str, end, 0);
+		else if constexpr (std::is_same_v<unsigned long, N>)
+			return std::strtoul(str, end, 0);
+		else if constexpr (std::is_same_v<unsigned long long, N>)
+			return std::strtoull(str, end, 0);
+		else if constexpr (std::is_signed_v<N>)
+		{
+			auto n = std::strtol(str, end, 0);
+			constexpr auto limit = range<N>::limit();
+			if(!limit.intersects(n))
+			{
+				errno = ERANGE;
+				return n < 0 ? limit.lower() : limit.upper();
+			}
+			return static_cast<N>(n);
+		}
+		else if constexpr (std::is_unsigned_v<N>)
+		{
+			auto n = std::strtoul(str, end, 0);
+			if(std::numeric_limits<N>::max() < n)
+			{
+				errno = ERANGE;
+				return std::numeric_limits<N>::max();
+			}
+			return static_cast<N>(n);
+		}
+	}
+
+	template <typename N>
+	N ston(const std::string& s, std::size_t * start_end_index)
+	{
+		static_assert(std::is_arithmetic_v<N>, "simple::support::ston expects an arithmetic type!");
+		errno = 0;
+		const char* start = s.c_str() + (start_end_index ? *start_end_index : 0);
+		char* end;
+		N result = strton<N>(start, &end);
+		if(ERANGE == errno)
+			throw std::out_of_range("simple::support::ston");
+		auto diff = end - start;
+		if(!diff)
+			throw std::invalid_argument("simple::support::ston");
+		if(start_end_index)
+			*start_end_index += diff;
+		return result;
+	}
+
+	template <typename N>
+	N ston(const std::string& s, std::size_t start_index = 0)
+	{
+		return ston<N>(s, &start_index);
+	}
+
+	template <typename N>
+	range<N> storn(const std::string& s, std::size_t * start_end_index)
+	{
+		static_assert(std::is_arithmetic_v<N>, "simple::support::storn expects an arithmetic type!");
+		range<N> result{};
+		std::size_t j = start_end_index ? *start_end_index : 0;
+		result.lower() = ston<N>(s, &j);
+		if(j == s.size())
+			throw std::invalid_argument("simple::support::storn");
+		auto delim = s[j];
+		++j;
+		result.upper() = ston<N>(s, &j);
+		if(':' == delim)
+			result.upper() += result.lower();
+		if(start_end_index)
+			*start_end_index = j;
+		return result;
+	}
+
+	template <typename N>
+	range<N> storn(const std::string& s, std::size_t start_index = 0)
+	{
+		return storn<N>(s, &start_index);
+	}
+
+	template <typename N>
+	std::string to_string(range<N> r, const char& separator = '-')
+	{
+		using std::to_string;
+		if(':' == separator)
+			r.upper() -= r.lower();
+		return to_string(r.lower()) + separator + to_string(r.upper());
+	}
+
+} // namespace simple::support
+
+#endif /* end of include guard */
---
+++ unit_tests/misc.cpp
@@ -0,0 +1,87 @@
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "simple/support/misc.hpp"
+
+using namespace simple::support;
+
+template <typename T>
+constexpr auto tos (T v) { using std::to_string; return to_string(v); }
+
+void StringToNumber()
+{
+	assert( 123 == ston<int>("123") );
+	assert( -0xabc == ston<int>("-0xabc") );
+	assert( -0123 == ston<int>("-0123") );
+	assert( 123.123f == ston<float>(tos(123.123f)) );
+	assert( -123.123 == ston<double>(tos(-123.123)) );
+	assert( 123.123l == ston<long double>(tos(123.123l)) );
+	assert( 123 == ston<int>("123xyz") );
+	assert( 23 == ston<int>("123xyz", 1) );
+	std::size_t i = 0;
+	assert( 123 == ston<int>("123xyz", &i) );
+	assert( 3 == i );
+	i = 1;
+	assert( 23 == ston<int>("123xyz", &i) );
+	assert( 3 == i );
+
+	try {
+		ston<int>("abc123");
+		assert( ((void)"ston did not throw an invalid argument exception", false) );
+	} catch(const std::invalid_argument & e) { }
+
+	try {
+		ston<char>(std::to_string(std::numeric_limits<char>::max()) + '0');
+		assert( ((void)"ston did not throw an out of range exception", false) );
+	} catch(const std::out_of_range & e) { }
+}
+
+void StringToNumericRange()
+{
+	assert( (range<int>{12,34} == storn<int>("12-34")) );
+	assert( (range<int>{12,24} == storn<int>("12:12")) );
+	assert( (range<int>{-0xcd,-0xab} == storn<int>("-0xcd,-0xab")) );
+	assert( (range<float>{12.12f,34.34f} == storn<float>(tos(12.12f) + '-' + tos(34.34f))) );
+	assert( (range<double>{-12.12,12.12} == storn<double>(tos(-12.12) + ':' + tos(24.24))) );
+	assert( (range<int>{12,34} == storn<int>("12-34.xyz")) );
+	assert( (range<int>{2,34} == storn<int>("12-34.xyz", 1)) );
+	std::size_t i = 0;
+	assert( (range<int>{12,34} == storn<int>("12-34.xyz", &i)) );
+	assert( 5 == i );
+	i = 1;
+	assert( (range<int>{2,34} == storn<int>("12-34.xyz", &i)) );
+	assert( 5 == i );
+
+	try {
+		storn<int>("1232");
+		assert( ((void)"storn did not throw an invalid argument exception", false) );
+	} catch(const std::invalid_argument & e) { }
+
+	try {
+		storn<int>("12: ");
+		assert( ((void)"storn did not throw an invalid argument exception", false) );
+	} catch(const std::invalid_argument & e) { }
+
+	try {
+		storn<int>("12 : 32");
+		assert( ((void)"storn did not throw an invalid argument exception", false) );
+	} catch(const std::invalid_argument & e) { }
+}
+
+void NumericRangeToString()
+{
+	assert( "12-34" == tos(range<int>{12,34}) );
+	assert( "12~34" == to_string(range<int>{12,34}, '~') );
+	assert( "12:34" == to_string(range<int>{12,46}, ':') );
+	assert( tos(-12.34) + '-' + tos(12.34) == tos(range<double>{-12.34,12.34}) );
+}
+
+int main()
+{
+	StringToNumber();
+	StringToNumericRange();
+	NumericRangeToString();
+	return 0;
+}

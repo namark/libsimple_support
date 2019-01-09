a3e4e7797c5f9dd358a0b97141d7932b78064c92
namark <namark@disroot.org>
2019-01-09 13:35:17 +0400

    Updated array operators
    to... do whatever .-.
    basically allowing compatible types with slightly stricter SFINAE
    I guess...

--- source/simple/support/array_operators.hpp
+++ source/simple/support/array_operators.hpp
@@ -46,94 +46,139 @@ namespace simple::support
 
 	template<> struct define_enum_flags_operators<array_operator> : public std::true_type {};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct add_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one += other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one += other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct sub_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one -= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one -= other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct mul_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one *= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one *= other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct div_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one /= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one /= other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct mod_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const
+		constexpr T1& operator()(T1& one, const T2& other) const
 		{
-			if constexpr (std::is_floating_point_v<Type>)
+			if constexpr (std::is_floating_point_v<T1> || std::is_floating_point_v<T2>)
 				return one = std::fmod(one, other);
 			else
 				return one %= other;
 		}
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
+	struct add
+	{
+		constexpr auto operator()(const T1& one, const T2& other) const { return one + other; }
+	};
+
+	template <typename T1, typename T2 = T1>
+	struct sub
+	{
+		constexpr auto operator()(const T1& one, const T2& other) const { return one - other; }
+	};
+
+	template <typename T1, typename T2 = T1>
+	struct mul
+	{
+		constexpr auto operator()(const T1& one, const T2& other) const { return one * other; }
+	};
+
+	template <typename T1, typename T2 = T1>
+	struct div
+	{
+		constexpr auto operator()(const T1& one, const T2& other) const { return one / other; }
+	};
+
+	template <typename T1, typename T2 = T1>
 	struct mod
 	{
-		constexpr Type operator()(const Type& one, const Type& other) const
+		constexpr auto operator()(const T1& one, const T2& other) const
 		{
-			if constexpr (std::is_floating_point_v<Type>)
+			if constexpr (std::is_floating_point_v<T1> || std::is_floating_point_v<T2>)
 				return std::fmod(one, other);
 			else
 				return one % other;
 		}
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct bit_and_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one &= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one &= other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct bit_or_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one |= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one |= other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct bit_xor_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one ^= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one ^= other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
+	struct bit_and
+	{
+		constexpr auto operator()(const T1& one, const T2& other) const { return one & other; }
+	};
+
+	template <typename T1, typename T2 = T1>
+	struct bit_or
+	{
+		constexpr auto operator()(const T1& one, const T2& other) const { return one | other; }
+	};
+
+	template <typename T1, typename T2 = T1>
+	struct bit_xor
+	{
+		constexpr auto operator()(const T1& one, const T2& other) const { return one ^ other; }
+	};
+
+	// sfinae on << and >> to not interfere with stream operators
+	// but ideally should do the same for the rest as well, to not interfere with anything
+	// so much duplicaion -_-
+	template <typename T1, typename T2 = T1, typename Result = decltype(std::declval<const T1&>() << std::declval<const T2&>())>
 	struct lshift
 	{
-		constexpr Type operator()(const Type& one, const Type& other) const { return one << other; }
+		constexpr Result operator()(const T1& one, const T2& other) const { return one << other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1, typename Result = decltype(std::declval<const T1&>() >> std::declval<const T2&>())>
 	struct rshift
 	{
-		constexpr Type operator()(const Type& one, const Type& other) const { return one >> other; }
+		constexpr Result operator()(const T1& one, const T2& other) const { return one >> other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct lshift_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one <<= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one <<= other; }
 	};
 
-	template <typename Type>
+	template <typename T1, typename T2 = T1>
 	struct rshift_in_place
 	{
-		constexpr Type& operator()(Type& one, const Type& other) const { return one >>= other; }
+		constexpr T1& operator()(T1& one, const T2& other) const { return one >>= other; }
 	};
 
 	template <typename Type>
@@ -234,6 +279,7 @@ namespace AOps_Details
 } // namespace simple::support
 
 // well, ended up with  macros after all... and still lots of duplication
+// either get rid of macros or make better use of them
 
 #define SIMPLE_SUPPORT_DEFINE_UNARY_OPERATOR(op_symbol, op_type, op_fun) \
 template <typename Array, \
@@ -265,39 +311,53 @@ constexpr Array operator op_symbol (const Array& one, const Array& other) \
 	return result; \
 } \
 \
-template <typename Array, \
+template <typename Array, typename Other, \
 	typename OperatorDef = simple::support::define_array_operators<Array>, \
-	std::enable_if_t<OperatorDef::enabled_right_element_operators && simple::support::array_operator::op_type>* = nullptr> \
+	typename Element = simple::support::AOps_Details::array_element_t<OperatorDef, Array>, \
+	typename Operator = op_fun<Element, Other>, \
+	std::enable_if_t<OperatorDef::enabled_right_element_operators && simple::support::array_operator::op_type \
+		&& !std::is_same_v<Array, Other> \
+	>* = nullptr, \
+	typename ElementResult = std::invoke_result_t<Operator, Element, Other>, \
+	std::enable_if_t<std::is_convertible_v<ElementResult, Element>>* =  nullptr \
+> \
 constexpr Array operator op_symbol \
 ( \
-	const Array& one, \
-	const simple::support::AOps_Details::array_element_t<OperatorDef, Array> & other \
+	const Array & one, \
+	const Other & other \
  ) \
 { \
 	using namespace simple::support; \
 	using namespace simple::support::AOps_Details; \
 	Array result{}; \
 	array_right_element_binary_operator \
-		<op_fun<array_element_t<OperatorDef, Array>>, OperatorDef::size> \
+		<Operator, OperatorDef::size> \
 		(OperatorDef::get_array(result), OperatorDef::get_array(one), other); \
 	return result; \
 } \
 \
-template <typename Array, \
+template <typename Array, typename Other, \
 	typename OperatorDef = simple::support::define_array_operators<Array>, \
-	std::enable_if_t<OperatorDef::enabled_left_element_operators && simple::support::array_operator::op_type>* = nullptr> \
+	typename Element = simple::support::AOps_Details::array_element_t<OperatorDef, Array>, \
+	typename Operator = op_fun<Other, Element>, \
+	std::enable_if_t<(OperatorDef::enabled_left_element_operators && simple::support::array_operator::op_type) \
+		&& !std::is_same_v<Array, Other> \
+	>* = nullptr, \
+	typename ElementResult = std::invoke_result_t<Operator,Other, Element>, \
+	std::enable_if_t<std::is_convertible_v<ElementResult, Element>>* =  nullptr \
+> \
 constexpr Array operator op_symbol \
 ( \
- 	const simple::support::AOps_Details::array_element_t<OperatorDef, Array> & one, \
-	const Array& other \
+	const Other & one, \
+	const Array & other \
 ) \
 { \
 	using namespace simple::support; \
 	using namespace simple::support::AOps_Details; \
 	Array result{}; \
 	array_left_element_binary_operator \
-		<op_fun<array_element_t<OperatorDef, Array>>, OperatorDef::size> \
-	(OperatorDef::get_array(result), OperatorDef::get_array(other), one); \
+		<Operator, OperatorDef::size> \
+		(OperatorDef::get_array(result), OperatorDef::get_array(other), one); \
 	return result; \
 }
 
@@ -314,62 +374,73 @@ constexpr Array& operator op_symbol (Array& one, const Array& other) \
 		(OperatorDef::get_array(one), OperatorDef::get_array(other)); \
 	return one; \
 } \
-template <typename Array, \
+template <typename Array, typename Other, \
 	typename OperatorDef = simple::support::define_array_operators<Array>, \
-	std::enable_if_t<OperatorDef::enabled_right_element_operators && simple::support::array_operator::op_type>* = nullptr> \
+	typename Element = simple::support::AOps_Details::array_element_t<OperatorDef, Array>, \
+	typename Operator = op_fun<Element, Other>, \
+	typename ElementResult = std::invoke_result_t<Operator, Element&, Other>, \
+	std::enable_if_t<OperatorDef::enabled_right_element_operators && simple::support::array_operator::op_type \
+		&& std::is_convertible_v<ElementResult, Element> \
+	>* = nullptr \
+> \
 constexpr Array& operator op_symbol \
 ( \
-	Array& one, \
-  	const simple::support::AOps_Details::array_element_t<OperatorDef, Array> & other \
+	Array & one, \
+	const Other & other \
 ) \
 { \
 	using namespace simple::support; \
 	using namespace simple::support::AOps_Details; \
 	array_right_element_in_place_operator \
-		<op_fun<array_element_t<OperatorDef, Array>>, OperatorDef::size> \
+		<Operator, OperatorDef::size> \
 		(OperatorDef::get_array(one), other); \
 	return one; \
 } \
-template <typename Array, \
+template <typename Array, typename Other, \
 	typename OperatorDef = simple::support::define_array_operators<Array>, \
-	std::enable_if_t<OperatorDef::enabled_left_element_operators && simple::support::array_operator::op_type>* = nullptr> \
-constexpr auto operator op_symbol \
+	typename Element = simple::support::AOps_Details::array_element_t<OperatorDef, Array>, \
+	typename Operator = op_fun<Other, Element>, \
+	typename ElementResult = std::invoke_result_t<Operator, Other&, Element>, \
+	std::enable_if_t<OperatorDef::enabled_left_element_operators && simple::support::array_operator::op_type \
+		&& std::is_convertible_v<ElementResult, Element> \
+	>* = nullptr \
+> \
+constexpr Other& operator op_symbol \
 ( \
-	simple::support::AOps_Details::array_element_t<OperatorDef, Array>& one, \
-	const Array& other \
+	Other & one, \
+	const Array & other \
 ) \
--> simple::support::AOps_Details::array_element_t<OperatorDef, Array> & \
 { \
 	using namespace simple::support; \
 	using namespace simple::support::AOps_Details; \
 	array_left_element_in_place_operator \
-		<op_fun<array_element_t<OperatorDef, Array>>, OperatorDef::size> \
+		<Operator, OperatorDef::size> \
 		(one, OperatorDef::get_array(other)); \
 	return one; \
 }
 
-SIMPLE_SUPPORT_DEFINE_UNARY_OPERATOR(~, bit_not, simple::support::bit_not);
-SIMPLE_SUPPORT_DEFINE_UNARY_OPERATOR(-, negate, std::negate);
-SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(+, add, std::plus)
-SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(-, sub, std::minus)
-SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(*, mul, std::multiplies)
-SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(/, div, std::divides)
+SIMPLE_SUPPORT_DEFINE_UNARY_OPERATOR(~, bit_not, simple::support::bit_not)
+SIMPLE_SUPPORT_DEFINE_UNARY_OPERATOR(-, negate, std::negate)
+SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(+, add, simple::support::add)
+SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(-, sub, simple::support::sub)
+SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(*, mul, simple::support::mul)
+SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(/, div, simple::support::div)
 SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(%, mod, simple::support::mod)
-SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(&, bit_and, std::bit_and)
-SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(|, bit_or, std::bit_or)
-SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(^, bit_xor, std::bit_xor)
+SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(&, bit_and, simple::support::bit_and)
+SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(|, bit_or, simple::support::bit_or)
+SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(^, bit_xor, simple::support::bit_xor)
 SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(<<, lshift, simple::support::lshift)
 SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR(>>, rshift, simple::support::rshift)
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(+=, add_eq, simple::support::add_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(-=, sub_eq, simple::support::sub_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(*=, mul_eq, simple::support::mul_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(/=, div_eq, simple::support::div_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(%=, mod_eq, simple::support::mod_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(&=, bit_and_eq, simple::support::bit_and_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(|=, bit_or_eq, simple::support::bit_or_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(^=, bit_xor_eq, simple::support::bit_xor_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(<<=, lshift_eq, simple::support::lshift_in_place);
-SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(>>=, rshift_eq, simple::support::rshift_in_place);
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(+=, add_eq, simple::support::add_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(-=, sub_eq, simple::support::sub_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(*=, mul_eq, simple::support::mul_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(/=, div_eq, simple::support::div_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(%=, mod_eq, simple::support::mod_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(&=, bit_and_eq, simple::support::bit_and_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(|=, bit_or_eq, simple::support::bit_or_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(^=, bit_xor_eq, simple::support::bit_xor_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(<<=, lshift_eq, simple::support::lshift_in_place)
+SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR(>>=, rshift_eq, simple::support::rshift_in_place)
 
 #undef SIMPLE_SUPPORT_DEFINE_BINARY_OPERATOR
 #undef SIMPLE_SUPPORT_DEFINE_IN_PLACE_OPERATOR
